a6dc26ac4999226085eb35701f034408cb05f736
Alex Gunnarsson <alexgu@kth.se>
2021-05-03 16:01:18 +0200

    Remove import GraphicsContext

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import javafx.animation.*;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
