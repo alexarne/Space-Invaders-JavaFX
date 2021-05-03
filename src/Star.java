6d75b05d7e34dfca476c496f36a9298da476c158
Alex Gunnarsson <alexgu@kth.se>
2021-05-04 00:43:51 +0200

    Small update to Star.java

--- src/Star.java
+++ src/Star.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Star {
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+/**
+ * A star, serves as part of the background
+ */
+public class Star extends Sprite {
+    Color color;
+
+    public Star(int posX, double posY, int height, int width, double velocity, Color color) {
+        super(posX, posY, height, width, velocity);
+        this.color = color;
+    }
+
+    /**
+     * Draw the star using current values, then update.
+     * @param gc The canvas to draw on.
+     */
+    public void draw(GraphicsContext gc) {
+        gc.setFill(color);
+        gc.fillRect(posX, posY, width, height);
+        this.posY += velocity;
+    }
 }
