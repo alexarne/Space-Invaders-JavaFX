2e1747811930401686071f6835bddc983c1a55ea
Alex Gunnarsson <alexgu@kth.se>
2021-05-04 16:26:57 +0200

    Enable future creation of .jar file

---
+++ src/Launcher.java
@@ -0,0 +1,10 @@
+// Instructions taken from:
+// https://taylorial.com/cs1021/Jar.htm
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Replace "Main" with the name of the class that extends Application
+        // See https://stackoverflow.com/a/52654791/3956070 for explanation
+        Main.main(args);
+    }
+}
