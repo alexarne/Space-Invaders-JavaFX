70aa8b211f06387e643af5b1befd570773db5669
joakimai <joakimai@kth.se>
2021-05-04 17:50:51 +0200

    Removed some enemies from the code, will start by implementing one

--- src/GameMechanics.java
+++ src/GameMechanics.java
@@ -27,8 +27,6 @@ public class GameMechanics {
     static final Image PLAYER_IMG = new Image("Assets/Images/rocketclean64.png");
 
     static final Image ENEMY1_IMG = new Image("Assets/Images/enemy1.png");
-    static final Image ENEMY2_IMG = new Image("Assets/Images/enemy2.png");
-    static final Image ENEMY3_IMG = new Image("Assets/Images/enemy3.png");
     private boolean ableToShoot;
 
     private Player player;
@@ -76,7 +74,7 @@ public class GameMechanics {
         player = new Player(WINDOW_WIDTH / 2 - 32, WINDOW_HEIGHT - 128, 64, 64, 10, PLAYER_IMG, 100, WINDOW_WIDTH);
 
         // Enemy(int posX, int posY, int height, int width, int velocity, Image img, int health, boolean ableToShoot, int windowHeight)
-        enemy = new Player(WINDOW_WIDTH / 2 - 32, WINDOW_HEIGHT, 64, 64, 10, PLAYER_IMG, 100, WINDOW_HEIGHT);
+        enemy = new Player(WINDOW_WIDTH / 2 - 32, WINDOW_HEIGHT, 64, 64, 10, ENEMY1_IMG, 100, WINDOW_HEIGHT);
 
         // Animate "the player" as if it's flying in from below
         ImageView playerSub = new ImageView(PLAYER_IMG);
