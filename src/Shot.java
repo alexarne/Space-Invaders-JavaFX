35280afafdd3498cb218cc7a9c153dd810014256
joakimai <joakimai@kth.se>
2021-05-04 19:46:58 +0200

    Refactored a method

--- src/Shot.java
+++ src/Shot.java
@@ -5,14 +5,14 @@ import javafx.scene.paint.Color;
  * Bullets (shots) from either the player or enemies.
  */
 public class Shot extends Sprite {
-    String origin; // ??
+    String origin; // String origin??
     Color color;
     static final int size = 8;
     public boolean toRemove;
 
-    public Shot(int posX, int posY, int height, int width, int velocity) { // String origin?
+    public Shot(int posX, int posY, int height, int width, int velocity) { // TODO: Add String origin
         super(posX, posY, height, width, velocity);
-        this.origin = origin;
+        this.origin = origin; // String origin
         this.color = Color.RED;
     }
 
@@ -30,14 +30,28 @@ public class Shot extends Sprite {
      */
     public void draw(GraphicsContext gc, int score) {
         gc.setFill(Color.ORANGE);
-        if (score >=50 && score<=70 || score>=120) {
-            gc.setFill(Color.YELLOWGREEN);
-            velocity = 50;
-            gc.fillRect(posX-5, posY-10, size+10, size+30);
+        if (scoreIsBetween50and70orGreaterThan120(score)) {
+            enableWeaponUpgrade(gc);
         } else {
-            gc.fillOval(posX, posY, size, size);
+            shootNormalBullets(gc);
         }
     }
+
+    private boolean scoreIsBetween50and70orGreaterThan120(int score) {
+        return score >= 50 && score <= 70 || score >= 120;
+    }
+
+    private void enableWeaponUpgrade(GraphicsContext gc) {
+        gc.setFill(Color.YELLOWGREEN);
+        velocity = 50;
+        gc.fillRect(posX-5, posY-10, size+10, size+30);
+    }
+
+    private void shootNormalBullets(GraphicsContext gc) {
+        gc.fillOval(posX, posY, size, size);
+    }
+
+    // TODO: Add method that handles collisions.
 
     public static int getSize() {
         return size;
